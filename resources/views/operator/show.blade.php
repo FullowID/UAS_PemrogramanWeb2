a896c6aaa15157f0e995639a9f593f34ac68a189
Rin <62451491+FullowID@users.noreply.github.com>
2022-06-06 21:39:46 +0800

    show op ganti ke versi card

--- resources/views/operator/show.blade.php
+++ resources/views/operator/show.blade.php
@@ -18,17 +18,38 @@
         </div>
         <div class="row mx-auto container overflow-hidden">
 
-        
             <div class="col-3 mx-auto">
-                <div class="p-2 text-white rounded bg-dark">Faction</div>
+                <p class="p-2 shadow text-white rounded bg-dark">Faction</p>
+                <div class="card shadow text-bg-light mb-3" style="max-width: 18rem;">
+                    <div class="card-body">
+                        <p>{{$operator->faction->factionname}}</p>
+                    </div>
+                </div>
             </div>
+
             <div class="col-3 mx-auto">
-                <div class="p-2 text-white rounded bg-dark">Race</div>
+                <p class="p-2 shadow text-white rounded bg-dark">Race</p>
+                <div class="card shadow text-bg-light" style="max-width: 18rem;">
+                    <div class="card-body">
+                        <p>{{$operator->race->name}}</p>
+                    </div>
+                </div>
             </div>
-            <div class="col-6 mx-auto">
-                <p class="p-2 text-white rounded bg-dark">Description</p>
+
+            <div class="col-5 mx-auto">
+                <p class="p-2 shadow text-white rounded bg-dark">Description</p>
+                <div class="card shadow text-bg-light" style="max-width: 28rem;">
+                    <div class="card-body">
+                        <p>{{$operator->description}}</p>
+                    </div>
+                </div>
             </div>
+
         </div>
+
     </div>
 </div>
+
+<div class="divider py-1"></div>
+
 @endsection
