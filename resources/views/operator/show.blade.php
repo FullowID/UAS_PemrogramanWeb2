da10b26170be4bc131800b9cfa789071139ae1e8
Fatur Rahman <2010817310018@mhs.ulm.ac.id>
2022-06-06 21:01:44 +0800

    final operator show

--- resources/views/operator/show.blade.php
+++ resources/views/operator/show.blade.php
@@ -3,38 +3,43 @@
 
 <div class="row">
     <div class="col-lg-12 m-3">
+        <div class="pull-left">
+            <h2> Show Operator</h2>
+        </div>
         <div class="pull-right">
             <a class="btn btn-primary" href="{{ route('operator.index') }}">Back</a>
         </div>
     </div>
 </div>
-
-<div class="text-secondary px-5 text-center">
-    <div class="py-1">
-        <h2 class="display-5 fw-bold text-white">{{ $operator->codename }}</h2>
-
-        <div class="shadow bg-body rounded">
-            <p class="fs-3">{{ $operator->operatorClass->classname }}</p>
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Codename:</strong>
+            {{ $operator->codename}}
         </div>
-        <div class="row mx-auto container overflow-hidden">
-
-        
-            <div class="col-6 mx-auto">
-                <div class="p-2 text-white rounded bg-dark">
-                    {{ $operator->faction->factionname }}
-                </div>
-            </div>
-            <div class="col-6 mx-auto">
-                <div class="p-2 text-white rounded bg-dark">
-                    {{ $operator->race->name }}
-                </div>
-            </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Class:</strong>
+            {{ $operator->operatorClass->classname}}
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Description:</strong>
-                {{ $operator->description}}
-            </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Factoion:</strong>
+            {{ $operator->faction->factionname}}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Race:</strong>
+            {{ $operator->race->name}}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Description:</strong>
+            {{ $operator->description}}
         </div>
     </div>
 </div>
