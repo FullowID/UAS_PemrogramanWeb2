f874d5feb25e2e57a8cb0d6c3bab7a8c6a9fba01
Rin <62451491+FullowID@users.noreply.github.com>
2022-06-06 22:06:31 +0800

    show faction

--- resources/views/faction/show.blade.php
+++ resources/views/faction/show.blade.php
@@ -1,31 +1,33 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Show Faction</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('faction.index') }}"> Back</a>
-            </div>
+<div class="row">
+    <div class="col-lg-12 m-3">
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('faction.index') }}">Back</a>
         </div>
     </div>
+</div>
 
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Faction Name:</strong>
-                {{ $faction->factionname }}
-            </div>
-        </div>
+<div class="text-secondary px-5 text-center">
+    <div class="py-1">
+        <h2 class="display-5 mb-5 fw-bold text-white">{{ $faction->factionname }}</h2>
 
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Faction Type:</strong>
-                {{ $faction->type }}
+        <div class="row mx-auto container overflow-hidden">
+
+
+            <div class="col-5 mx-auto">
+                <p class="p-2 shadow text-white rounded bg-dark">Type</p>
+                <div class="card shadow text-bg-light" style="max-width: 28rem;">
+                    <div class="card-body">
+                        <p>{{$faction->type}}</p>
+                    </div>
+                </div>
             </div>
+
         </div>
 
     </div>
+</div>
+
 @endsection
